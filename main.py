5b04a7631b6919b776c98a30faf657db8ab4f80e
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-11-13 18:41:38 +0100

    create util func to print logs

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from stats import get_character_count, get_words_count
+from typing import Literal
+
+from stats import Char_Count_Dict, get_character_count, get_words_count
 
 FRANKENSTEIN_BOOK_PATH = "books/frankenstein.txt"
+
+Decoration = Literal["bookbot", "word-count", "char-count"]
 
 
 def get_book_text(file_path: str) -> str:
@@ -9,12 +13,31 @@ def get_book_text(file_path: str) -> str:
         return file_contents
 
 
+def get_text_decorate(decoration: Decoration):
+    print_message: dict[Decoration, str] = {
+        "bookbot": "============ BOOKBOT ============",
+        "word-count": "----------- Word Count ----------",
+        "char-count": "--------- Character Count -------",
+    }
+
+    return print_message[decoration]
+
+
+def print_logs(count: int, char_count: Char_Count_Dict):
+    print(get_text_decorate("bookbot"))
+    print(f"Analyzing book found at {FRANKENSTEIN_BOOK_PATH}")
+    print(get_text_decorate("word-count"))
+    print(f"Found {count} total words")
+    print(get_text_decorate("char-count"))
+    print(char_count)
+
+
 def main() -> None:
     file = get_book_text(FRANKENSTEIN_BOOK_PATH)
     count = get_words_count(file)
     char_count = get_character_count(file)
-    print(f"Found {count} total words")
-    print(char_count)
+
+    print_logs(count, char_count)
 
 
 main()
