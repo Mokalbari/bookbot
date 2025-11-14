dc1a81e0020f2669524362263c047d0b8f29bc7e
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-11-14 18:11:48 +0100

    add dynamic path parsing

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import sys
 from typing import Literal
 
-from stats import Char_Count_Dict, get_character_count, get_words_count, sort_dict
+from stats import get_character_count, get_words_count, sort_dict
 
-FRANKENSTEIN_BOOK_PATH = "books/frankenstein.txt"
+MIN_SYS_ARG = 2
 
 Decoration = Literal["bookbot", "word-count", "char-count", "end"]
+
+
+def check_sys_argv():
+    if len(sys.argv) < MIN_SYS_ARG:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 
 def get_book_text(file_path: str) -> str:
@@ -24,9 +31,9 @@ def get_text_decorate(decoration: Decoration):
     return print_message[decoration]
 
 
-def print_logs(count: int, char_count: list[tuple[str, int]]):
+def print_logs(count: int, book_path: str, char_count: list[tuple[str, int]]):
     print(get_text_decorate("bookbot"))
-    print(f"Analyzing book found at {FRANKENSTEIN_BOOK_PATH}")
+    print(f"Analyzing book found at {book_path}")
     print(get_text_decorate("word-count"))
     print(f"Found {count} total words")
     print(get_text_decorate("char-count"))
@@ -41,12 +48,14 @@ def print_logs(count: int, char_count: list[tuple[str, int]]):
 
 
 def main() -> None:
-    file = get_book_text(FRANKENSTEIN_BOOK_PATH)
+    check_sys_argv()
+    book_path = sys.argv[1]
+    file = get_book_text(book_path)
     count = get_words_count(file)
     char_count = get_character_count(file)
     sorted = sort_dict(char_count)
 
-    print_logs(count, sorted)
+    print_logs(count, book_path, sorted)
 
 
 main()
