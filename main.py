bf7966f7c323243db14a88bd5b8a2e1beddbd784
mokalbari <150693283+Mokalbari@users.noreply.github.com>
2025-11-14 18:12:43 +0100

    reorder file

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ MIN_SYS_ARG = 2
 Decoration = Literal["bookbot", "word-count", "char-count", "end"]
 
 
+def main() -> None:
+    check_sys_argv()
+    book_path = sys.argv[1]
+    file = get_book_text(book_path)
+    count = get_words_count(file)
+    char_count = get_character_count(file)
+    sorted = sort_dict(char_count)
+
+    print_logs(count, book_path, sorted)
+
+
 def check_sys_argv():
     if len(sys.argv) < MIN_SYS_ARG:
         print("Usage: python3 main.py <path_to_book>")
@@ -45,17 +56,6 @@ def print_logs(count: int, book_path: str, char_count: list[tuple[str, int]]):
         print(f"{char[0]}: {char[1]}")
 
     print(get_text_decorate("end"))
-
-
-def main() -> None:
-    check_sys_argv()
-    book_path = sys.argv[1]
-    file = get_book_text(book_path)
-    count = get_words_count(file)
-    char_count = get_character_count(file)
-    sorted = sort_dict(char_count)
-
-    print_logs(count, book_path, sorted)
 
 
 main()
